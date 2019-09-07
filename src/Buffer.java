478cee0cd7848dabb23a6cf9f3b3e31364fc8d4a
Luis Miguel Gomez Londono <luismigolondo@gmail.com>
2019-09-07 13:08:01 -0500

    Lectura de archivo

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,4 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
 /**
  * 
@@ -10,10 +17,16 @@ import java.util.ArrayList;
  */
 public class Buffer {
 
-	private int tamanio;
+	private static int tamanio;
 	
-	private ArrayList<Mensaje> mensajes;
+	private static int numeroClientes;
 	
+	private static int numeroServidores;
+	
+	private static int consultasClientes;
+
+	private ArrayList<Mensaje> mensajes = new ArrayList<>();
+
 	public Mensaje asignarMensaje() {
 		// TODO Auto-generated method stub
 		return null;
@@ -21,6 +34,61 @@ public class Buffer {
 
 	public void guardar(Mensaje men) {
 		// TODO Auto-generated method stub
+
+	}
+
+	public static void leerArchivo()
+	{
+		try {
+			JFileChooser jFileChooser = new JFileChooser();
+			jFileChooser.setCurrentDirectory(new File("./data/"));
+			jFileChooser.setDialogTitle("Seleccione el archivo de configuracion del Caso");
+			int result = jFileChooser.showOpenDialog(new JFrame());
+			File selectedFile = null;
+			if (result == JFileChooser.APPROVE_OPTION) {
+				selectedFile = jFileChooser.getSelectedFile();
+				System.out.println("Archivo seleccionado: " + selectedFile.getAbsolutePath());
+			}
+			BufferedReader br = new BufferedReader(new FileReader(selectedFile));
+			br.readLine();
+			br.readLine();
+			String linea = br.readLine();
+			
+			String [] l = linea.split(",");
+			
+			numeroClientes = (int) Integer.parseInt(l[0]);
+			numeroServidores = (int) Integer.parseInt(l[1]);
+			consultasClientes = (int) Integer.parseInt(l[2]);
+			tamanio = (int) Integer.parseInt(l[3]);
+
+			br.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Caso 1 Infracomp - 2019-2");
+		System.out.println("Luis Miguel Gomez Londono - 201729597");
+		System.out.println("Daniel Bernal - #########");
+		System.out.println("");
+		leerArchivo();
+		System.out.println("Numero de Clientes: " + numeroClientes);
+		System.out.println("Numero de Servidores: " + numeroServidores);
+		System.out.println("Numero de consultas: " + consultasClientes);
+		System.out.println("Tamanio BUFFER: " + tamanio);	
+		
+		System.out.println();
+		
+		System.out.println("*******************************************");
+		System.out.println("*                 WORKING                 *");
+		System.out.println("*******************************************");
+
 		
 	}
 
